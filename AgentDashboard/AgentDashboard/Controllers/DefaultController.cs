55ccf82fed6bcfc2a5cbb06d917572965a5da8d9
wangzhizhe <wzz80@163.com>
2018-07-09 23:09:11 +0800

    餐厅开店闭店更新

--- AgentDashboard/AgentDashboard/Controllers/DefaultController.cs
+++ AgentDashboard/AgentDashboard/Controllers/DefaultController.cs
@@ -1,28 +1,95 @@
 ﻿using AgentDashboard.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace AgentDashboard.Controllers
 {
     public class DefaultController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        private int GetStartRowNo(int pageIndex, int pageSize)
+        {
+            if ((pageIndex <= 0) || (pageSize <= 0))
+            {
+                return 0;
+            }
+
+            return (pageIndex - 1) * pageSize;
+        }
+
         // GET: Default
         public ActionResult Index()
         {
-            SPEntities sp = new SPEntities();
-            var shopList = sp.SP_Shop.ToList();
-            List<ShopViewModel> shopsVM = shopList.Select(x => new ShopViewModel
+            List<ShopViewModel> shopsVM = null;
+            using (SPEntities sp = new SPEntities())
             {
-                Id = x.Id,
-                ShopName = x.ShopName,
-                ShopType = x.ShopType,
-                ShopStatus = x.ShopStatus
-            }).ToList();
+                var shopList = sp.SP_Shop.OrderByDescending(n => n.RegionId).OrderByDescending(n=>n.Id).Skip(GetStartRowNo(1, 20)).Take(20).ToList();
+                shopsVM = shopList.Select(x => new ShopViewModel
+                {
+                    Id = x.Id,
+                    ShopName = x.ShopName,
+                    ShopType = x.ShopType,
+                    ShopStatus = x.ShopStatus
+                }).ToList();
+            }
 
             return View(shopsVM);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UpdateShopStatus()
+        {
+            SPEntities spEntity = new SPEntities();
+            var sr = new StreamReader(Request.InputStream);
+            var stream = sr.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var list = js.Deserialize<List<ShopViewModel>>(stream);
+
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    DbContextTransaction transcation = null;
+
+                    try
+                    {
+                        var shop = spEntity.SP_Shop.Where(n => n.Id == item.Id).FirstOrDefault();
+
+                        transcation = spEntity.Database.BeginTransaction();
+                        shop.ShopStatus = item.ShopStatus;
+                        spEntity.SaveChanges();
+                        transcation.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transcation.Rollback();
+                        throw;
+                    }
+                    finally
+                    {
+                        if (transcation != null) transcation.Dispose();
+                    }
+                }
+            }
+            
+            return Json(String.Empty);
         }
 
         // GET: Default
