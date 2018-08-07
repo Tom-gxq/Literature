fcac80cb12d589bea6e5925cb2f4fe3084ea1519
wangzhizhe <wzz80@163.com>
2018-08-07 23:05:27 +0800

    jquery ajax extend getValues

--- AgentDashboard/AgentDashboard/js/sb-admin.js
+++ AgentDashboard/AgentDashboard/js/sb-admin.js
@@ -42,4 +42,21 @@
     }, 1000, 'easeInOutExpo');
     event.preventDefault();
   });
-})(jQuery); // End of use strict
+})(jQuery);
+
+jQuery.extend({
+    getValues: function (url) {
+        var result = null;
+        $.ajax({
+            url: url,
+            type: 'get',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            async: false,
+            success: function (data) {
+                result = data;
+            }
+        });
+        return result;
+    }
+});// End of use strict
