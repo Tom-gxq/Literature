ac4234ba7f21f101e8932c154b3e4ddaff4521cc
wangzhizhe <wzz80@163.com>
2018-12-16 14:23:00 +0800

    商家管理页面 ShopManagerViewModel漏传

---
+++ AgentDashboard/AgentDashboard/Models/ShopManagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgentDashboard.Models
+{
+    public class ShopManagerViewModel
+    {
+        public List<SellerViewModel> Sellers;
+        public Dictionary<int, String> Universities { get; set; }
+        public int SelectIndex { get; set; }
+    }
+}
