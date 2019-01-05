22f232d74bb49e2bc4f9f479bf61ceed039580a0
wangzhizhe <wzz80@163.com>
2019-01-05 11:54:00 +0800

    regionaccount & suppliersRegion database entity and respository author:gxq

---
+++ SP.DataEntity34/RegionAccountEntity.cs
@@ -0,0 +1,19 @@
+﻿using LibMain.Domain.Entities;
+using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.DataEntity
+{
+    [Alias("SP_RegionAccount")]
+    public class RegionAccountEntity : Entity<string>
+    {
+        [Alias("AccountId")]
+        public override string Id { get; set; }
+        public int? RegionId { get; set; }
+        public int? Status { get; set; }
+    }
+}
---
+++ SP.ManageEntityFramework/Repositories/RegionAccountRespository.cs
@@ -0,0 +1,34 @@
+﻿using Lib.EntityFramework.EntityFramework;
+using ServiceStack.OrmLite;
+using SP.DataEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.ManageEntityFramework.Repositories
+{
+    public class RegionAccountRespository : RepositoryBase<RegionAccountEntity, string>
+    {
+        public RegionAccountRespository(IDbContextProvider<ManageDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+        public bool Add(RegionAccountEntity data)
+        {
+            var result = this.Insert(data);
+            return result > 0;
+        }
+
+        public List<RegionAccountEntity> GetRegionAccount( int pageIndex, int pageSize)
+        {
+            using (var db = Context.OpenDbConnection())
+            {
+                var q = db.From<RegionAccountEntity>().Where(x => x.Status == 0);
+                q = q.Limit((pageIndex - 1) * pageSize, pageSize);
+                return db.Select(q);
+            }
+        }
+    }
+}
